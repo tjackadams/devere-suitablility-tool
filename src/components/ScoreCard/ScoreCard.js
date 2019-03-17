1283bdae4e48b1df40747a9dafcbba3be0115941
Thomas Adams <tjackadams@itadams.co.uk>
2019-03-17 19:44:04 +0000

    moved contact details to the top of the page.
    #2

--- src/components/ScoreCard/ScoreCard.js
+++ src/components/ScoreCard/ScoreCard.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import { Icon } from "semantic-ui-react";
 import { Container, Header, Segment } from "semantic-ui-react";
 import { Accordion } from "semantic-ui-react";
@@ -19,6 +19,27 @@ const style = {
   }
 };
 
+const ContactInformation = ({ state }) => {
+  let text;
+  switch (state) {
+    case "Ok":
+      text =
+        "You have a strong case to obtain a UK Ex-pat mortgage, but if you would like to discuss further ";
+      break;
+    default:
+      text = "";
+  }
+
+  const contact =
+    "Please contact the deVere Mortgage Team on +44 3333449510, or alternatively email us to book in for a free telephone consulatation at mortgages@devere-mortgages.co.uk";
+
+  return <Header as="h4" >
+  <Header.Content>
+  {text}<br />{contact}
+  </Header.Content>
+  </Header>;
+};
+
 const ScoreCard = ({ answers }) => {
   console.log("answers", answers);
 
@@ -26,6 +47,7 @@ const ScoreCard = ({ answers }) => {
   let icon;
   let iconColor;
   let headerText;
+  let contactInformation;
 
   const errors = getAnswers(answers, "Error");
   if (errors.length > 0) {
@@ -33,6 +55,7 @@ const ScoreCard = ({ answers }) => {
     icon = "question circle outline";
     iconColor = "red";
     headerText = "Limited Lenders Available";
+    contactInformation = <ContactInformation state="Error" />;
   } else {
     const warnings = getAnswers(answers, "Warning");
     if (warnings.length > 0) {
@@ -40,11 +63,13 @@ const ScoreCard = ({ answers }) => {
       icon = "warning circle";
       iconColor = "yellow";
       headerText = "Limited Lenders Available";
+      contactInformation = <ContactInformation state="Warning" />;
     } else {
       result = getAnswers(answers, "Ok");
       icon = "check circle";
       iconColor = "green";
       headerText = "Strong Case to Proceed";
+      contactInformation = <ContactInformation state="Ok" />;
     }
   }
 
@@ -58,6 +83,7 @@ const ScoreCard = ({ answers }) => {
               <Header.Content>{headerText}</Header.Content>
             </Header>
           </Segment>
+          <Segment textAlign="center">{contactInformation}</Segment>
           <Segment>
             <Accordion
               panels={[
