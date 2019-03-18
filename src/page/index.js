d25595df8668ca31783effe8004c5b6d7f2f720e
Thomas Adams <tjackadams@itadams.co.uk>
2019-03-18 12:00:35 +0000

    added route animation

--- src/page/index.js
+++ src/page/index.js
@@ -1,14 +1,28 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import posed, { PoseGroup } from "react-pose";
 
 import Home from "./Home";
 import Result from "./Result";
 
+const RoutesContainer = posed.div({
+  enter: { opacity: 1, delay: 300 },
+  exit: { opacity: 0 }
+});
+
 const Page = () => (
-  <>
-    <Route path="/" exact component={Home} />
-    <Route path="/result" component={Result} />
-  </>
+  <Route
+    render={({ location }) => (
+      <PoseGroup>
+        <RoutesContainer key={location.pathname}>
+          <Switch location={location} key="switch">
+            <Route path="/" exact component={Home} key="home" />
+            <Route path="/result" component={Result} key="result" />
+          </Switch>
+        </RoutesContainer>
+      </PoseGroup>
+    )}
+  />
 );
 
 export default Page;
